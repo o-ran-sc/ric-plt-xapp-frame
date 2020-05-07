df85a6535c56b9eb2f9e364202f8fa5915ba64b3
Juha Hyttinen <juha.hyttinen@nokia.com>
2020-05-07 12:42:26 +0300

    Support to prometheus CounterVec and GaugeVec.
    Change-Id: I053340aa148ab10499dfab7ec4f83aa94a811e31
    Signed-off-by: Juha Hyttinen <juha.hyttinen@nokia.com>

--- pkg/xapp/metrics.go
+++ pkg/xapp/metrics.go
@@ -31,7 +31,7 @@ type Metrics struct {
 }
 
 // Alias
-type CounterOpts prometheus.CounterOpts
+type CounterOpts prometheus.Opts
 type Counter prometheus.Counter
 type Gauge prometheus.Gauge
 
@@ -51,9 +51,11 @@ func NewMetrics(url, namespace string, r *mux.Router) *Metrics {
 	return &Metrics{Namespace: namespace}
 }
 
-func (m *Metrics) RegisterCounter(opts CounterOpts) Counter {
+/*
+ * Handling counters
+ */
+func (m *Metrics) registerCounter(opts CounterOpts) Counter {
 	Logger.Info("Register new counter with opts: %v", opts)
-
 	return promauto.NewCounter(prometheus.CounterOpts(opts))
 }
 
@@ -62,15 +64,17 @@ func (m *Metrics) RegisterCounterGroup(opts []CounterOpts, subsytem string) (c m
 	for _, opt := range opts {
 		opt.Namespace = m.Namespace
 		opt.Subsystem = subsytem
-		c[opt.Name] = m.RegisterCounter(opt)
+		c[opt.Name] = m.registerCounter(opt)
 	}
 
 	return
 }
 
-func (m *Metrics) RegisterGauge(opts CounterOpts) Gauge {
+/*
+ * Handling gauges
+ */
+func (m *Metrics) registerGauge(opts CounterOpts) Gauge {
 	Logger.Info("Register new gauge with opts: %v", opts)
-
 	return promauto.NewGauge(prometheus.GaugeOpts(opts))
 }
 
@@ -79,8 +83,107 @@ func (m *Metrics) RegisterGaugeGroup(opts []CounterOpts, subsytem string) (c map
 	for _, opt := range opts {
 		opt.Namespace = m.Namespace
 		opt.Subsystem = subsytem
-		c[opt.Name] = m.RegisterGauge(opt)
+		c[opt.Name] = m.registerGauge(opt)
 	}
 
+	return
+}
+
+/*
+ * Handling counter vectors
+ *
+ * Example:
+
+	vec := xapp.Metric.RegisterCounterVecGroup(
+		[]xapp.CounterOpts{
+			{Name: "counter1", Help: "counter1"},
+			{Name: "counter2", Help: "counter2"},
+		},
+		[]string{"host"},
+		"SUBSYSTEM")
+
+	stat:=xapp.Metric.GetCounterGroupFromVec(vec, []string{"localhost:8888"})
+
+*/
+type CounterVec struct {
+	Vec  *prometheus.CounterVec
+	Opts CounterOpts
+}
+
+func (m *Metrics) registerCounterVec(opts CounterOpts, labelNames []string) *prometheus.CounterVec {
+	Logger.Info("Register new counter vector with opts: %v labelNames: %v", opts, labelNames)
+
+	return promauto.NewCounterVec(prometheus.CounterOpts(opts), labelNames)
+}
+
+func (m *Metrics) RegisterCounterVecGroup(opts []CounterOpts, labelNames []string, subsytem string) (c map[string]CounterVec) {
+	c = make(map[string]CounterVec)
+	for _, opt := range opts {
+		entry := CounterVec{}
+		entry.Opts = opt
+		entry.Opts.Namespace = m.Namespace
+		entry.Opts.Subsystem = subsytem
+		entry.Vec = m.registerCounterVec(entry.Opts, labelNames)
+		c[opt.Name] = entry
+	}
+	return
+}
+
+func (m *Metrics) GetCounterGroupFromVec(vec map[string]CounterVec, labels []string) (c map[string]Counter) {
+	c = make(map[string]Counter)
+	for name, opt := range vec {
+		c[name] = opt.Vec.WithLabelValues(labels...)
+		Logger.Info("Register new counter for vector with opts: %v labels: %v", opt.Opts, labels)
+	}
+	return
+}
+
+/*
+ * Handling gauge vectors
+ *
+ * Example:
+
+	vec := xapp.Metric.RegisterGaugeVecGroup(
+		[]xapp.CounterOpts{
+			{Name: "gauge1", Help: "gauge1"},
+			{Name: "gauge2", Help: "gauge2"},
+		},
+		[]string{"host"},
+		"SUBSYSTEM")
+
+	stat:=xapp.Metric.GetGaugeGroupFromVec(vec, []string{"localhost:8888"})
+
+*/
+type GaugeVec struct {
+	Vec  *prometheus.GaugeVec
+	Opts CounterOpts
+}
+
+func (m *Metrics) registerGaugeVec(opts CounterOpts, labelNames []string) *prometheus.GaugeVec {
+	Logger.Info("Register new gauge vector with opts: %v labelNames: %v", opts, labelNames)
+
+	return promauto.NewGaugeVec(prometheus.GaugeOpts(opts), labelNames)
+}
+
+func (m *Metrics) RegisterGaugeVecGroup(opts []CounterOpts, labelNames []string, subsytem string) (c map[string]GaugeVec) {
+	c = make(map[string]GaugeVec)
+	for _, opt := range opts {
+		entry := GaugeVec{}
+		entry.Opts = opt
+		entry.Opts.Namespace = m.Namespace
+		entry.Opts.Subsystem = subsytem
+		entry.Vec = m.registerGaugeVec(entry.Opts, labelNames)
+		c[opt.Name] = entry
+
+	}
+	return
+}
+
+func (m *Metrics) GetGaugeGroupFromVec(vec map[string]GaugeVec, labels []string) (c map[string]Gauge) {
+	c = make(map[string]Gauge)
+	for name, opt := range vec {
+		c[name] = opt.Vec.WithLabelValues(labels...)
+		Logger.Info("Register new gauge for vector with opts: %v labels: %v", opt.Opts, labels)
+	}
 	return
 }
